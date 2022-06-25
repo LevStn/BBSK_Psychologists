bb594dbbf364e8dc94997492d3ab3dd5a71e069a
Emil <intestinal_d@rambler.ru>
2022-06-25 17:37:37 +0300

    Initialized ManagerController
    Also added few API-methods

---
+++ Controllers/ManagerController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BBSK_Psycho.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class ManagerController : Controller
+    {
+        private readonly ILogger<ManagerController> _logger;
+
+
+        public ManagerController(ILogger<ManagerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{orders}")]
+        public Client GetAllOrders()
+        {
+            return null!;
+        }
+
+        [HttpGet("{orders}/{id}")]
+        public Client GetOrderById(int id)
+        {
+            return null!;
+        }
+
+        [HttpGet("{orders}/{sessions}")]
+        public Client GetSessionById(int id)
+        {
+            return null!;
+        }
+
+        [HttpGet("{orders}/{sessions}/{id}")]
+        public Client GetAllSessions(int id)
+        {
+            return null!;
+        }
+    }
+}
