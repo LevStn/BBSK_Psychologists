71e9260aa9f0d32dccc1eeca1fdfd674098f72cb
Daria Shadrina <darinashadrina@yandex.ru>
2022-06-25 18:06:51 +0300

    Initialized PsychologistController

---
+++ Controllers/PsychologistsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BBSK_Psycho.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PsychologistsController : ControllerBase
+    {
+        private readonly ILogger<PsychologistsController> _logger;
+
+
+        public PsychologistsController(ILogger<PsychologistsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{id}")]
+        public Psychologist GetPsychologistById(int id)
+        {
+            return new Psychologist();
+        }
+        [HttpGet()]
+        public List<Psychologist> GetPsychologists()
+        { 
+            return new List<Psychologist>();
+        }
+    }
+}
