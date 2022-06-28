43e50c4092369964155b3a154ca4c2abb579276e
Daria Shadrina <darinashadrina@yandex.ru>
2022-06-29 00:53:47 +0300

    Add model for psychologist

--- Models/Responses/GetPsychologistResponse.cs
+++ Models/Responses/GetPsychologistResponse.cs
@@ -2,5 +2,38 @@
 {
     public class GetPsychologistResponse
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public string Sex { get; set; }
+
+        public DateOnly BirthDate { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public int WorkExperience { get; set; }
+
+        public string PasportData { get; set; }
+
+        public List<string> Education { get; set; }  // "2013 - Московский Государственный Университет - Факультет - Степень; Dev Education"
+
+        public int Status { get; set; }        //Enum
+
+        public List<string>? TherapyMethods { get; set; }
+
+        public List<string>? Problems { get; set; }
+
+        public decimal Price { get; set; }
+
+        public Dictionary<DateOnly, List<TimeOnly>> Schedule { get; set; }
+
+        public string DenyMessage { get; set; }
     }
 }
