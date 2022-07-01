5c77a3b4988d81c3c6a6acd2b96a068c1255f1db
Lev <leva.100s@gmail.com>
2022-07-01 16:04:51 +0300

    Add Required for Order

--- Models/Requests/OrderCreateRequest.cs
+++ Models/Requests/OrderCreateRequest.cs
@@ -1,27 +1,41 @@
 ﻿using BBSK_Psycho.Enums;
+using BBSK_Psycho.Infrastructure;
+using System.ComponentModel.DataAnnotations;
 
 namespace BBSK_Psycho.Models;
 
 public class OrderCreateRequest
 {
+    [Required(ErrorMessage = ApiErrorMessage.ClientIdIsRequired)]
     public int ClientId { get; set; }
 
+    [Required]
     public int PsychologistId { get; set; }
 
+    [Required(ErrorMessage = ApiErrorMessage.CostIsRequired)]
     public decimal Cost { get; set; }
 
+    [Required(ErrorMessage = ApiErrorMessage.DurationIsRequired)]
     public int Duration { get; set; }
 
+    [Required(ErrorMessage = ApiErrorMessage.MessageIsRequired)]
     public string Message { get; set; }
 
+    [Required(ErrorMessage = ApiErrorMessage.SessionDateIsRequired)]
+    [DataType(DataType.Date, ErrorMessage = ApiErrorMessage.InvalidDate)]
     public DateTime SessionDate { get; set; }
 
+    [Required(ErrorMessage = ApiErrorMessage.SessionDateIsRequired)]
+    [DataType(DataType.Date, ErrorMessage = ApiErrorMessage.OrderDateIsRequired)]
     public DateTime OrderDate { get; set; }
 
+    [DataType(DataType.Date, ErrorMessage = ApiErrorMessage.OrderDateIsRequired)]
     public DateTime? PayDate { get; set; }
 
+    [Required(ErrorMessage = ApiErrorMessage.OrderStatusIsRequired)]
     public OrderStatus OrderStatus { get; set; }
 
+    [Required(ErrorMessage = ApiErrorMessage.OrderPaymentStatusIsRequired)]
     public OrderPaymentSatus OrderPaymentStatus { get; set; }
 }
 
