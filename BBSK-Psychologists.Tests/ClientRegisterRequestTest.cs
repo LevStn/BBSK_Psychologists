0dab974db0d893e8d9ae0ae7148cc01d42007542
Lev <leva.100s@gmail.com>
2022-07-04 14:55:45 +0300

    Add Required tests for ClintRegister

---
+++ BBSK-Psychologists.Tests/ClientRegisterRequestTest.cs
@@ -0,0 +1,22 @@
+﻿using BBSK_Psycho.Models;
+using BBSK_Psychologists.Tests.ModelControllerSource;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BBSK_Psychologists.Tests;
+
+public class ClientRegisterRequestTest
+{
+
+    [TestCaseSource(typeof(ClientRegisterRequestTestsSourceForRequired))]
+    public void WhenEmailNamePasswordAreEmptyShouldThrowException(ClientRegisterRequest client, string errorMessage)
+    {
+        var validationsResults = new List<ValidationResult>();
+        var isValid = Validator.TryValidateObject(client, new ValidationContext(client), validationsResults);
+        Assert.IsFalse(isValid);
+        var actualMessage = validationsResults[0].ErrorMessage;
+        Assert.AreEqual(errorMessage, actualMessage);
+    }
+}
