98b92bd8fa035304e81ff1148db07524e647df18
Daria Shadrina <darinashadrina@yandex.ru>
2022-07-05 12:20:19 +0300

    Delete extra tests

--- BBSK-Psychologists.Tests/PsychologistAddRequestTests.cs
+++ BBSK-Psychologists.Tests/PsychologistAddRequestTests.cs
@@ -15,132 +15,13 @@ namespace BBSK_Psychologists.Tests
         }
 
         [TestCaseSource(typeof(PsychologistAddRequestTestSource))]
-        public void TestPasswordLengthIsLess(AddPsychologistRequest addPsychologistRequest, string errorMessage)
+        public void PsychologistAddRequestTest(AddPsychologistRequest addPsychologistRequest, string errorMessage)
         {
             var validationResultList = new List<ValidationResult>();
             var isValid = Validator.TryValidateObject(addPsychologistRequest, new ValidationContext(addPsychologistRequest), validationResultList, true);
             var message = validationResultList[0].ErrorMessage;
             Assert.AreEqual(errorMessage, message);
 
-        }
-
-        [TestCaseSource(typeof(PsychologistAddRequestTestSource))]
-        public void TestNameIsRequired(AddPsychologistRequest addPsychologistRequest, string errorMessage)
-        {
-            var validationResultList = new List<ValidationResult>();
-            var isValid = Validator.TryValidateObject(addPsychologistRequest, new ValidationContext(addPsychologistRequest), validationResultList, true);
-            var message = validationResultList[0].ErrorMessage;
-            Assert.AreEqual(errorMessage, message);
-
-        }
-
-        [TestCaseSource(typeof(PsychologistAddRequestTestSource))]
-        public void TestPasswordIsRequire(AddPsychologistRequest addPsychologistRequest, string errorMessage)
-        {
-            var validationResultList = new List<ValidationResult>();
-            var isValid = Validator.TryValidateObject(addPsychologistRequest, new ValidationContext(addPsychologistRequest), validationResultList, true);
-            var message = validationResultList[0].ErrorMessage;
-            Assert.AreEqual(errorMessage, message);
-
-        }
-
-        [TestCaseSource(typeof(PsychologistAddRequestTestSource))]
-        public void TestEmailIsRequire(AddPsychologistRequest addPsychologistRequest, string errorMessage)
-        {
-            var validationResultList = new List<ValidationResult>();
-            var isValid = Validator.TryValidateObject(addPsychologistRequest, new ValidationContext(addPsychologistRequest), validationResultList, true);
-            var message = validationResultList[0].ErrorMessage;
-            Assert.AreEqual(errorMessage, message);
-
-        }
-        [TestCaseSource(typeof(PsychologistAddRequestTestSource))]
-        public void TestLastNameIsRequired(AddPsychologistRequest addPsychologistRequest, string errorMessage)
-        {
-            var validationResultList = new List<ValidationResult>();
-            var isValid = Validator.TryValidateObject(addPsychologistRequest, new ValidationContext(addPsychologistRequest), validationResultList, true);
-            var message = validationResultList[0].ErrorMessage;
-            Assert.AreEqual(errorMessage, message);
-
-        }
-        [TestCaseSource(typeof(PsychologistAddRequestTestSource))]
-        public void TestPatronymicIsRequired(AddPsychologistRequest addPsychologistRequest, string errorMessage)
-        {
-            var validationResultList = new List<ValidationResult>();
-            var isValid = Validator.TryValidateObject(addPsychologistRequest, new ValidationContext(addPsychologistRequest), validationResultList, true);
-            var message = validationResultList[0].ErrorMessage;
-            Assert.AreEqual(errorMessage, message);
-
-        }
-        [TestCaseSource(typeof(PsychologistAddRequestTestSource))]
-        public void TestPsychologistGenderIsRequired(AddPsychologistRequest addPsychologistRequest, string errorMessage)
-        {
-            var validationResultList = new List<ValidationResult>();
-            var isValid = Validator.TryValidateObject(addPsychologistRequest, new ValidationContext(addPsychologistRequest), validationResultList, true);
-            var message = validationResultList[0].ErrorMessage;
-            Assert.AreEqual(errorMessage, message);
-
-        }
-
-        [TestCaseSource(typeof(PsychologistAddRequestTestSource))]
-        public void TestBirthDateIsRequired(AddPsychologistRequest addPsychologistRequest, string errorMessage)
-        {
-            var validationResultList = new List<ValidationResult>();
-            var isValid = Validator.TryValidateObject(addPsychologistRequest, new ValidationContext(addPsychologistRequest), validationResultList, true);
-            var message = validationResultList[0].ErrorMessage;
-            Assert.AreEqual(errorMessage, message);
-
-        }
-
-        [TestCaseSource(typeof(PsychologistAddRequestTestSource))]
-        public void TestPhineNumberIsRequired(AddPsychologistRequest addPsychologistRequest, string errorMessage)
-        {
-            var validationResultList = new List<ValidationResult>();
-            var isValid = Validator.TryValidateObject(addPsychologistRequest, new ValidationContext(addPsychologistRequest), validationResultList, true);
-            var message = validationResultList[0].ErrorMessage;
-            Assert.AreEqual(errorMessage, message);
-        }
-         [TestCaseSource(typeof(PsychologistAddRequestTestSource))]
-        public void WorkExperienceIsRequired(AddPsychologistRequest addPsychologistRequest, string errorMessage)
-        {
-            var validationResultList = new List<ValidationResult>();
-            var isValid = Validator.TryValidateObject(addPsychologistRequest, new ValidationContext(addPsychologistRequest), validationResultList, true);
-            var message = validationResultList[0].ErrorMessage;
-            Assert.AreEqual(errorMessage, message);
-        }
-
-        [TestCaseSource(typeof(PsychologistAddRequestTestSource))]
-        public void PassportDataIsRequired(AddPsychologistRequest addPsychologistRequest, string errorMessage)
-        {
-            var validationResultList = new List<ValidationResult>();
-            var isValid = Validator.TryValidateObject(addPsychologistRequest, new ValidationContext(addPsychologistRequest), validationResultList, true);
-            var message = validationResultList[0].ErrorMessage;
-            Assert.AreEqual(errorMessage, message);
-        }
-
-        [TestCaseSource(typeof(PsychologistAddRequestTestSource))]
-        public void EducationIsRequired(AddPsychologistRequest addPsychologistRequest, string errorMessage)
-        {
-            var validationResultList = new List<ValidationResult>();
-            var isValid = Validator.TryValidateObject(addPsychologistRequest, new ValidationContext(addPsychologistRequest), validationResultList, true);
-            var message = validationResultList[0].ErrorMessage;
-            Assert.AreEqual(errorMessage, message);
-        }
-        [TestCaseSource(typeof(PsychologistAddRequestTestSource))]
-        public void TherapyMethodsIsRequired(AddPsychologistRequest addPsychologistRequest, string errorMessage)
-        {
-            var validationResultList = new List<ValidationResult>();
-            var isValid = Validator.TryValidateObject(addPsychologistRequest, new ValidationContext(addPsychologistRequest), validationResultList, true);
-            var message = validationResultList[0].ErrorMessage;
-            Assert.AreEqual(errorMessage, message);
-        }
-
-        [TestCaseSource(typeof(PsychologistAddRequestTestSource))]
-        public void ProblemsIsRequired(AddPsychologistRequest addPsychologistRequest, string errorMessage)
-        {
-            var validationResultList = new List<ValidationResult>();
-            var isValid = Validator.TryValidateObject(addPsychologistRequest, new ValidationContext(addPsychologistRequest), validationResultList, true);
-            var message = validationResultList[0].ErrorMessage;
-            Assert.AreEqual(errorMessage, message);
         }
        
     }
