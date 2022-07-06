3ec3e8b30cc259e681cce96d4fea8aad23d3f617
Daria Shadrina <darinashadrina@yandex.ru>
2022-07-06 15:58:57 +0300

    Add Psy and Schedule and rebase enums

---
+++ BBSK_Psycho.DataLayer/Entities/Psychologist.cs
@@ -0,0 +1,45 @@
+﻿using BBSK_Psycho.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBSK_Psycho.DataLayer.Entities
+{
+    public class Psychologist
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+        
+        public string Patronymic { get; set; }
+
+        public Gender gender { get; set; }
+   
+        public DateTime? BirthDate { get; set; }
+        
+        public string Phone { get; set; }
+        
+        public string Password { get; set; }
+        
+        public string Email { get; set; }
+
+        public int? WorkExperience { get; set; }
+
+        public string PasportData { get; set; }
+
+        public List<string>? Education { get; set; }
+        
+        public CheckStatus checkStatus { get; set; }
+       
+        public List<string>? TherapyMethods { get; set; }
+       
+        public List<string>? Problems { get; set; }
+      
+        public decimal Price { get; set; }
+        
+    }
+}
---
+++ BBSK_Psycho.DataLayer/Entities/Schedule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBSK_Psycho.DataLayer.Entities
+{
+    public class Schedule
+    {
+        public int Id { get; set; }
+        public DateTime Date  { get; set; }
+        public DateTime Time  { get; set; }
+    }
+}
