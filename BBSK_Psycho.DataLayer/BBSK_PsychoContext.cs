b48db7cadf39664f900a18550afdb0cace05c7b4
Lev <leva.100s@gmail.com>
2022-07-06 17:47:22 +0300

    Revert "Add changes setup for BBSK_PsychoContext"
    This reverts commit c3fc62afe18f12a16521fea3ac933d67c5a2ca71.

--- BBSK_Psycho.DataLayer/BBSK_PsychoContext.cs
+++ BBSK_Psycho.DataLayer/BBSK_PsychoContext.cs
@@ -60,14 +60,7 @@ public class BBSK_PsychoContext : DbContext
 
 
 
-
         });
-
-        modelBuilder.Entity<Psychologist>()
-            .Property(p => p.Price)
-            .HasPrecision(18, 2);
-
-
 
         modelBuilder.Entity<Schedule>(entity =>
         {
---
+++ BBSK_Psychologists/RequestForSearch.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace BBSK_Psycho
+{
+    public class RequestForSearch
+    {
+        public int ClientId { get; set; }
+
+        public int ServiceType { get; set; } //тип услуги (одиночная, семейная)
+
+        public List<string> Problems { get; set; }
+    }
+}
