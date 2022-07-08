3ea1a73358954a33f676e27cd374d6c026105b9d
Daria Shadrina <darinashadrina@yandex.ru>
2022-07-08 20:55:15 +0300

    Add class for PsyRepository

---
+++ BBSK_Psycho.DataLayer/Repositories/PsychologistsRepository.cs
@@ -0,0 +1,21 @@
+﻿using BBSK_Psycho.DataLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBSK_Psycho.DataLayer.Repositories
+{
+    public class PsychologistsRepository
+    {
+        private readonly BBSK_PsychoContext _context;
+
+        public PsychologistsRepository (BBSK_PsychoContext context)
+        {
+            _context = context;
+        }
+
+        public Psychologist? GetPsychologist(int id) => _context.Psychologists.FirstOrDefault (p => p.Id == id);
+    }
+}
