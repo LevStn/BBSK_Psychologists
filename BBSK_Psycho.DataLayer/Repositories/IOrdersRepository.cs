b1cfbd5f711338ae979fe5209cef3086c6307a39
Emil <intestinal_d@rambler.ru>
2022-07-11 02:51:40 +0300

    Initialized OrdersRepository and IOrdersRepository interface based on it

---
+++ BBSK_Psycho.DataLayer/Repositories/IOrdersRepository.cs
@@ -0,0 +1,13 @@
+﻿using BBSK_Psycho.DataLayer.Entities;
+
+namespace BBSK_Psycho.DataLayer.Repositories
+{
+    public interface IOrdersRepository
+    {
+        int AddOrder(Order order);
+        void DeleteOrderById(int id);
+        List<Order> GetAllOrders();
+        Order? GetOrderById(int id);
+        void UpdateOrderStatusById(int orderId, int orderStatus, int paymentStatus);
+    }
+}
---
+++ BBSK_Psycho.DataLayer/Repositories/OrdersRepository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BBSK_Psycho.DataLayer.Entities;
+using BBSK_Psycho.DataLayer.Enums;
+
+namespace BBSK_Psycho.DataLayer.Repositories
+{
+    public class OrdersRepository : IOrdersRepository
+    {
+        private readonly BBSK_PsychoContext _context;
+
+        public OrdersRepository(BBSK_PsychoContext context)
+        {
+            _context = context;
+        }
+
+        public List<Order> GetAllOrders()
+        {
+            return _context.Orders.Where(o => o.IsDeleted == false).ToList();
+        }
+
+        public Order? GetOrderById(int id)
+        {
+            return _context.Orders.FirstOrDefault(o => o.Id == id);
+        }
+
+        public int AddOrder(Order order)
+        {
+            _context.Orders.Add(order);
+            _context.SaveChanges();
+
+            return order.Id;
+        }
+
+        public void DeleteOrderById(int id)
+        {
+            Order order = GetOrderById(id);
+
+            order.IsDeleted = true;
+
+            _context.SaveChanges();
+        }
+
+        public void UpdateOrderStatusById(int orderId, int orderStatus, int paymentStatus)
+        {
+            if (orderStatus > Enum.GetNames(typeof(OrderStatus)).Length ||
+                paymentStatus > Enum.GetNames(typeof(OrderPaymentStatus)).Length)
+            {
+                throw new ArgumentException("Invalid arguments");
+            }
+
+            Order order = GetOrderById(orderId);
+            order.OrderStatus = (OrderStatus)orderStatus;
+            order.OrderPaymentStatus = (OrderPaymentStatus)paymentStatus;
+
+            _context.Orders.Update(order);
+            _context.SaveChanges();
+        }
+    }
+}
