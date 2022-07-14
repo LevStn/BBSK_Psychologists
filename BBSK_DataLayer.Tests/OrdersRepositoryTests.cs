7e73d17c737670b51641f82b37a7633c618ce3ce
Emil <intestinal_d@rambler.ru>
2022-07-15 00:01:33 +0300

    Initialized DataLayer.Tests nUnit project
    And OrdersRepositoryTest

---
+++ BBSK_DataLayer.Tests/OrdersRepositoryTests.cs
@@ -0,0 +1,22 @@
+using BBSK_Psycho.DataLayer;
+using BBSK_Psycho.DataLayer.Entities;
+using BBSK_Psycho.DataLayer.Repositories;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace BBSK_DataLayer.Tests
+{
+    public class OrdersRepositoryTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
