e388cf3a1b50e896d73769cdea0dcb5b4bb83919
Emil <intestinal_d@rambler.ru>
2022-07-15 03:49:15 +0300

    Added GetOrders method test

--- BBSK_DataLayer.Tests/OrdersRepositoryTests.cs
+++ BBSK_DataLayer.Tests/OrdersRepositoryTests.cs
@@ -3,20 +3,69 @@ using BBSK_Psycho.DataLayer.Entities;
 using BBSK_Psycho.DataLayer.Repositories;
 using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
+using BBSK_Psycho.DataLayer.Enums;
+using BBSK_DataLayer.Tests.TestCaseSources;
 
 namespace BBSK_DataLayer.Tests
 {
     public class OrdersRepositoryTests
     {
+        private DbContextOptions<BBSK_PsychoContext> _dbContextOptions;
+        private BBSK_PsychoContext _context;
+        private OrdersRepository _sut;
+
+        public OrdersRepositoryTests()
+        {
+            _dbContextOptions = new DbContextOptionsBuilder<BBSK_PsychoContext>()
+                .UseInMemoryDatabase(databaseName: "TestingDB")
+                .Options;
+        }
+
+
+
+
         [SetUp]
         public void Setup()
         {
+            if(_context != null)
+            {
+                _context.Database.EnsureDeleted();
+            }
+
+            _context = new BBSK_PsychoContext(_dbContextOptions);
+
+            _sut = new OrdersRepository(_context);
         }
 
         [Test]
-        public void Test1()
+        public void GetOrders_WhenCorrectEndpointCalled_ThenOrdersListReturned()
         {
-            Assert.Pass();
+            //given
+
+            Client client = OrdersHelper.GetClient();
+            Psychologist psychologist = OrdersHelper.GetPsychologist();
+            Order firstOrder = OrdersHelper.GetOrder(client, psychologist);
+            Order secondOrder = OrdersHelper.GetOrder();
+
+            _context.Orders.Add(firstOrder);
+            _context.Orders.Add(secondOrder);
+            _context.SaveChanges();
+
+            //when
+            List<Order> orders = _sut.GetOrders();
+
+
+            //then
+            Assert.NotNull(orders);
+            Assert.That(orders != null);
+
+            Assert.That(orders, Contains.Item(firstOrder));
+            Assert.That(orders, Contains.Item(secondOrder));
+
+            Assert.That(firstOrder.ClientId == client.Id);
+            Assert.That(firstOrder.Client.Id == client.Id);
+            Assert.That(firstOrder.PsychologistId == psychologist.Id);
+            Assert.That(firstOrder.Psychologist.Id == psychologist.Id);
         }
     }
 }
