fcfef18d907abc907291b3d05b770f52d06c3f2c
Daria Shadrina <darinashadrina@yandex.ru>
2022-07-19 16:24:23 +0300

    Copy the class with middleware from the branch of Lev

--- BBSK_Psychologists/Middleware/CustomExceptionHandlerMiddleware.cs
+++ BBSK_Psychologists/Middleware/CustomExceptionHandlerMiddleware.cs
@@ -1,6 +1,74 @@
-﻿namespace BBSK_Psycho.Middleware
+﻿using System.Text.Json;
+
+namespace BBSK_Psycho.Middleware
 {
     public class CustomExceptionHandlerMiddleware
     {
+        private readonly RequestDelegate _next;
+
+        public CustomExeptionHandlerMiddleware(RequestDelegate next) =>
+            _next = next;
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (EntityNotFoundException exception)
+            {
+                await HandleExceptionAsync(context, exception);
+            }
+            catch (UniquenessException exception)
+            {
+                await HandleExceptionAsync(context, exception);
+            }
+            catch (DataException exception)
+            {
+                await HandleExceptionAsync(context, exception);
+            }
+            catch (AccessException exception)
+            {
+                await HandleExceptionAsync(context, exception);
+            }
+
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var code = HttpStatusCode.InternalServerError;
+            var result = string.Empty;
+
+            switch (exception)
+            {
+                case ValidationException validationException:
+                    code = HttpStatusCode.BadRequest;
+                    result = JsonSerializer.Serialize(validationException.Message);
+                    break;
+                case EntityNotFoundException:
+                    code = HttpStatusCode.NotFound;
+                    break;
+                case UniquenessException:
+                    code = HttpStatusCode.UnprocessableEntity;
+                    break;
+                case DataException:
+                    code = HttpStatusCode.UnprocessableEntity;
+                    break;
+                case AccessException:
+                    code = HttpStatusCode.Forbidden;
+                    break;
+
+            }
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+
+            if (result == string.Empty)
+            {
+                result = JsonSerializer.Serialize(new { error = exception.Message });
+            }
+
+            return context.Response.WriteAsync(result);
+        }
     }
 }
