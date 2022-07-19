5dd3114acd2b0ffc1347de42cc6311324c2fcbf8
Daria Shadrina <darinashadrina@yandex.ru>
2022-07-19 19:53:08 +0300

    Add Psychologist service and interface

--- BBSK_Psycho.BusinessLayer/Services/Interfaces/IPsychologistServices.cs
+++ BBSK_Psycho.BusinessLayer/Services/Interfaces/IPsychologistServices.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BBSK_Psycho.DataLayer.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace BBSK_Psycho.BusinessLayer.Services.Interfaces
 {
-    internal class IPsychologistServices
+    public interface IPsychologistServices
     {
+        Psychologist? GetPsychologist(int id);
+        List<Psychologist> GetAllPsychologists();
+        public Comment AddCommentToPsyhologist(Comment comment, int psychologistId);
+        List<Comment> GetCommentsByPsychologistId(int id);
+        int AddPsychologist(Psychologist psychologist);
+        void UpdatePsychologist(Psychologist psychologist, int id);
+        void DeletePsychologist(int id);
     }
 }
--- BBSK_Psycho.BusinessLayer/Services/PsychologistServices.cs
+++ BBSK_Psycho.BusinessLayer/Services/PsychologistServices.cs
@@ -1,7 +1,43 @@
-﻿namespace BBSK_Psycho.BusinessLayer
-{
-    public class PsychologistServices
-    {
+﻿using BBSK_Psycho.BusinessLayer.Services.Interfaces;
+using BBSK_Psycho.DataLayer.Entities;
 
+namespace BBSK_Psycho.BusinessLayer
+{
+    public class PsychologistServices : IPsychologistServices
+    {
+        public Comment AddCommentToPsyhologist(Comment comment, int psychologistId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int AddPsychologist(Psychologist psychologist)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePsychologist(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Psychologist> GetAllPsychologists()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Comment> GetCommentsByPsychologistId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Psychologist? GetPsychologist(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePsychologist(Psychologist psychologist, int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
