a9f5199137f0a2da15f567d42f6db8a83861f6d5
Emil <intestinal_d@rambler.ru>
2022-07-20 05:09:53 +0300

    Initialized OrdersService and it's interface

---
+++ BBSK_Psycho.BusinessLayer/Services/Interfaces/IOrdersService.cs
@@ -0,0 +1,13 @@
+﻿using BBSK_Psycho.DataLayer.Entities;
+
+namespace BBSK_Psycho.BusinessLayer.Services.Interfaces
+{
+    public interface IOrdersService
+    {
+        int AddOrder();
+        void DeleteOrderById();
+        List<Order> GetAllOrders();
+        Order? GetOrderById();
+        void UpdateOrderStatusById();
+    }
+}
---
+++ BBSK_Psycho.BusinessLayer/Services/OrdersService.cs
@@ -0,0 +1,18 @@
+﻿using BBSK_Psycho.BusinessLayer.Services.Interfaces;
+using BBSK_Psycho.DataLayer.Entities;
+
+namespace BBSK_Psycho.BusinessLayer.Services
+{
+    public class OrdersService : IOrdersService
+    {
+        public List<Order> GetAllOrders() { return null; }
+
+        public Order? GetOrderById() { return null; }
+
+        public int AddOrder() { return 0; }
+
+        public void DeleteOrderById() { }
+
+        public void UpdateOrderStatusById() { }
+    }
+}
