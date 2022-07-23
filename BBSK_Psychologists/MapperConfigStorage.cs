e2009776d8e6349412696246719474a289b6b922
Daria Shadrina <darinashadrina@yandex.ru>
2022-07-24 02:55:22 +0300

    Make space

--- BBSK_Psychologists/MapperConfigStorage.cs
+++ BBSK_Psychologists/MapperConfigStorage.cs
@@ -8,24 +8,24 @@ namespace BBSK_Psycho;
 
 public class MapperConfigStorage : Profile
 {
-	public MapperConfigStorage()
-	{
-		CreateMap<ClientRegisterRequest, Client>();
-		CreateMap<Client, ClientResponse>();
-		CreateMap<Comment, CommentResponse>();
-		CreateMap<Psychologist, PsychologistResponse>();
-		CreateMap<Psychologist, GetAllPsychologistsResponse>();
-		CreateMap<AddPsychologistRequest, Psychologist>()
-			.ForMember(x => x.Educations, s => s.MapFrom(x => x.Educations!.Select(education => new Education() { EducationData = education })))
-			.ForMember(x => x.Problems, s => s.MapFrom(x => x.Problems!.Select(problemName => new Problem() { ProblemName = problemName })))
-			.ForMember(x => x.TherapyMethods, s => s.MapFrom(x => x.TherapyMethods!.Select(therapyMethod => new TherapyMethod() { Method = therapyMethod })));
-		CreateMap<UpdatePsychologistRequest, Psychologist>()
-			.ForMember(x => x.Educations, s => s.MapFrom(x => x.Educations!.Select(education => new Education() { EducationData = education })))
-			.ForMember(x => x.Problems, s => s.MapFrom(x => x.Problems!.Select(problemName => new Problem() { ProblemName = problemName })))
-			.ForMember(x => x.TherapyMethods, s => s.MapFrom(x => x.TherapyMethods!.Select(therapyMethod => new TherapyMethod() { Method = therapyMethod })));
-		CreateMap<Comment, GetCommentsByPsychologistIdResponse>();
-		CreateMap<CommentRequest, Comment>()
-			.ForMember(x => x.Psychologist, s => s.MapFrom((x => (new Psychologist { Id= x.PsychologistId}))))
-			.ForMember(x => x.Client, s => s.MapFrom((x => (new Client { Id= x.ClientId}))));
-	}
+    public MapperConfigStorage()
+    {
+        CreateMap<ClientRegisterRequest, Client>();
+        CreateMap<Client, ClientResponse>();
+        CreateMap<Comment, CommentResponse>();
+        CreateMap<Psychologist, PsychologistResponse>();
+        CreateMap<Psychologist, GetAllPsychologistsResponse>();
+        CreateMap<AddPsychologistRequest, Psychologist>()
+            .ForMember(x => x.Educations, s => s.MapFrom(x => x.Educations!.Select(education => new Education() { EducationData = education })))
+            .ForMember(x => x.Problems, s => s.MapFrom(x => x.Problems!.Select(problemName => new Problem() { ProblemName = problemName })))
+            .ForMember(x => x.TherapyMethods, s => s.MapFrom(x => x.TherapyMethods!.Select(therapyMethod => new TherapyMethod() { Method = therapyMethod })));
+        CreateMap<UpdatePsychologistRequest, Psychologist>()
+            .ForMember(x => x.Educations, s => s.MapFrom(x => x.Educations!.Select(education => new Education() { EducationData = education })))
+            .ForMember(x => x.Problems, s => s.MapFrom(x => x.Problems!.Select(problemName => new Problem() { ProblemName = problemName })))
+            .ForMember(x => x.TherapyMethods, s => s.MapFrom(x => x.TherapyMethods!.Select(therapyMethod => new TherapyMethod() { Method = therapyMethod })));
+        CreateMap<Comment, GetCommentsByPsychologistIdResponse>();
+        CreateMap<CommentRequest, Comment>()
+            .ForMember(x => x.Psychologist, s => s.MapFrom((x => (new Psychologist { Id= x.PsychologistId}))))
+            .ForMember(x => x.Client, s => s.MapFrom((x => (new Client { Id= x.ClientId}))));
+    }
 }
