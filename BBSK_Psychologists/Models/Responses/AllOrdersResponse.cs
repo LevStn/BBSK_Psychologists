fd2bef3653b3726fa2bd9d23fe34e607ca0193b7
Emil <intestinal_d@rambler.ru>
2022-07-29 22:56:03 +0300

    Added new model
    AllOrdersResponse using as List<AllOrdersResonse> in GetOrders() controller method

---
+++ BBSK_Psychologists/Models/Responses/AllOrdersResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BBSK_Psycho.DataLayer.Enums;
+
+namespace BBSK_Psycho.Models.Responses
+{
+    public class AllOrdersResponse
+    {
+        public int Id { get; set; }
+        public decimal Cost { get; set; }
+        public SessionDuration Duration { get; set; }
+        public string Message { get; set; }
+        public DateTime SessionDate { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime PayDate { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public OrderPaymentStatus OrderPaymentStatus { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
