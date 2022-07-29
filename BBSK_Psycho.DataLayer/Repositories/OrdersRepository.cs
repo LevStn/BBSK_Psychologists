597947477f0232611626802ac52892267c3567f2
Emil <intestinal_d@rambler.ru>
2022-07-29 22:56:18 +0300

    Updated OrdersRepository

--- BBSK_Psycho.DataLayer/Repositories/OrdersRepository.cs
+++ BBSK_Psycho.DataLayer/Repositories/OrdersRepository.cs
@@ -1,6 +1,7 @@
 ﻿using BBSK_Psycho.DataLayer.Entities;
 using BBSK_Psycho.DataLayer.Enums;
 using BBSK_Psycho.DataLayer.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace BBSK_Psycho.DataLayer.Repositories
 {
@@ -13,10 +14,10 @@ namespace BBSK_Psycho.DataLayer.Repositories
             _context = context;
         }
 
-        public List<Order> GetOrders() => _context.Orders.Where(o => !o.IsDeleted).ToList();
+        public List<Order> GetOrders() => _context.Orders.AsNoTracking().Where(o => !o.IsDeleted).ToList();
         
 
-        public Order? GetOrderById(int id) => _context.Orders.FirstOrDefault(o => o.Id == id);
+        public Order? GetOrderById(int orderId) => _context.Orders.Include(o => o.Client).Include(o => o.Psychologist).FirstOrDefault(o => o.Id == orderId);
         
 
         public int AddOrder(Order order)
@@ -27,20 +28,21 @@ namespace BBSK_Psycho.DataLayer.Repositories
             return order.Id;
         }
 
-        public void DeleteOrder(int id)
+        public void DeleteOrder(int orderId)
         {
-            Order order = _context.Orders.FirstOrDefault(o => o.Id == id);
+            Order order = _context.Orders.FirstOrDefault(o => o.Id == orderId);
 
             order.IsDeleted = true;
 
             _context.SaveChanges();
         }
 
-        public void UpdateOrderStatus(int orderId, OrderStatus orderStatus, OrderPaymentStatus paymentStatus)
+        public void UpdateOrderStatuses(int orderId, OrderStatus orderStatus, OrderPaymentStatus orderPaymentStatus)
         {
-            Order order = GetOrderById(orderId);
-            order.OrderStatus = (OrderStatus)orderStatus;
-            order.OrderPaymentStatus = (OrderPaymentStatus)paymentStatus;
+            Order order = _context.Orders.FirstOrDefault(o => o.Id == orderId);
+
+            order.OrderStatus = orderStatus;
+            order.OrderPaymentStatus = orderPaymentStatus;
 
             _context.Orders.Update(order);
             _context.SaveChanges();
