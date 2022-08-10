9b855ddbf9080c45c8990fa13a79b50356a32ad6
Emil <intestinal_d@rambler.ru>
2022-08-11 00:52:06 +0300

    Fixed OrdersController tests

--- BBSK-Psychologists.Tests/OrdersControllerTests.cs
+++ BBSK-Psychologists.Tests/OrdersControllerTests.cs
@@ -13,12 +13,14 @@ using BBSK_Psycho.BusinessLayer.Services.Interfaces;
 using BBSK_Psycho.BusinessLayer;
 using BBSK_Psycho;
 using BBSK_Psycho.Models.Responses;
+using System.Security.Claims;
 
 namespace BBSK_Psychologists.Tests
 {
     public class OrdersControllerTests
     {
         private OrdersController _sut;
+        private Mock<OrdersController> _ordersController;
         private Mock<IOrdersService> _ordersService;
         private ClaimModel _claimModel;
         private IMapper _mapper;
@@ -28,25 +30,30 @@ namespace BBSK_Psychologists.Tests
         public void Setup()
         {
             _mapper = new Mapper(new MapperConfiguration(cfg => cfg.AddProfile<MapperConfigStorage>()));
+            _ordersController = new Mock<OrdersController>();
             _ordersService = new Mock<IOrdersService>();
             _sut = new OrdersController(_ordersService.Object, _mapper);
             _claimModel = new ClaimModel();
+
+            ClaimsPrincipal user = new();
+            _sut.ControllerContext = new ControllerContext();
+            _sut.ControllerContext.HttpContext = new DefaultHttpContext() { User = user };
         }
 
         [Test]
-        public void GetOrders_NoValidationRequired_RequestedTypeReceived()
+        public void GetOrders_ValidRolePassed_RequestedTypeReceived()
         {
             //given
-            List<Order> allOrders = new List<Order>();
+            List<Order> allOrders = new List<Order>() { OrdersHelper.GetOrder(), OrdersHelper.GetOrder() };
+            
+            _claimModel.Role = Role.Manager;
 
-            allOrders.Add(OrdersHelper.GetOrder());
-            allOrders.Add(OrdersHelper.GetOrder());
+            _sut.ControllerContext.HttpContext.User = OrdersHelper.GetUser("go@v.no", Role.Manager, 58);
 
-            _ordersService.Setup(c => c.GetOrders(It.IsAny<ClaimModel>())).Returns(allOrders);
+            _ordersService.Setup(c => c.GetOrders(_claimModel)).Returns(allOrders);
 
             //when
             var actual = _sut.GetOrders();
-
 
             //then
             var actualResult = actual.Result as ObjectResult;
@@ -55,7 +62,7 @@ namespace BBSK_Psychologists.Tests
             Assert.AreEqual(StatusCodes.Status200OK, actualResult.StatusCode);
             
             Assert.IsNotNull(actual);
-            
+
             for(int i = 0; i < allOrders.Count; i++)
             {
                 Assert.AreEqual(actualValues[i].Duration, allOrders[i].Duration);
@@ -68,16 +75,22 @@ namespace BBSK_Psychologists.Tests
                 Assert.AreEqual(actualValues[i].SessionDate, allOrders[i].SessionDate);
             }
 
-            _ordersService.Verify(c => c.GetOrders(It.IsAny<ClaimModel>()), Times.Once);
+            _ordersService.Verify(c => c.GetOrders(_claimModel), Times.Once);
         }
 
-        [Test]
-        public void GetOrderById_ValidIdPassed_OkReceived()
+        [TestCase(Role.Manager)]
+        [TestCase(Role.Client)]
+        [TestCase(Role.Psychologist)]
+        public void GetOrderById_ValidIdPassed_OkReceived(Role role)
         {
             //given
-            var expectedOrder = OrdersHelper.GetOrder();
+            Order expectedOrder = OrdersHelper.GetOrder();
 
-            _ordersService.Setup(c => c.GetOrderById(expectedOrder.Id, It.IsAny<ClaimModel>())).Returns(expectedOrder);
+            _claimModel.Role = role;
+
+            _ordersService.Setup(c => c.GetOrderById(expectedOrder.Id, _claimModel)).Returns(expectedOrder);
+
+            _sut.ControllerContext.HttpContext.User = OrdersHelper.GetUser("go@v.no", role, 58);
 
             //when
             var actual = _sut.GetOrderById(expectedOrder.Id);
@@ -93,23 +106,29 @@ namespace BBSK_Psychologists.Tests
             Assert.AreEqual((SessionDuration)actualValue.Duration, expectedOrder.Duration);
             Assert.AreEqual(actualValue.Message, expectedOrder.Message);
             Assert.AreEqual(actualValue.OrderDate, expectedOrder.OrderDate);
-            Assert.AreEqual((OrderPaymentStatus)actualValue.OrderPaymentStatus, expectedOrder.OrderPaymentStatus);
+            Assert.AreEqual(actualValue.OrderPaymentStatus, expectedOrder.OrderPaymentStatus);
             Assert.AreEqual(actualValue.OrderStatus, expectedOrder.OrderStatus);
             Assert.AreEqual(actualValue.PayDate, expectedOrder.PayDate);
             Assert.AreEqual(actualValue.SessionDate, expectedOrder.SessionDate);
 
-            _ordersService.Verify(c => c.GetOrderById(It.IsAny<int>(), It.IsAny<ClaimModel>()), Times.Once);
+            _ordersService.Verify(c => c.GetOrderById(expectedOrder.Id, _claimModel), Times.Once);
         }
 
-        [Test]
-        public void AddOrder_ValidRequestPassed_CreatedResultReceived()
+        [TestCase(Role.Manager)]
+        [TestCase(Role.Client)]
+        public void AddOrder_ValidRequestPassed_CreatedResultReceived(Role role)
         {
             //given
-            _ordersService.Setup(c => c.AddOrder(It.IsAny<Order>(), It.IsAny<ClaimModel>()));
+            _claimModel.Role = role;
+
+            _sut.ControllerContext.HttpContext.User = OrdersHelper.GetUser("go@v.no", role, 58);
+
+            Client client = OrdersHelper.GetClient();
+            Psychologist psychologist = OrdersHelper.GetPsychologist();
 
             OrderCreateRequest givenRequest = new()
             {
-                ClientId = 1,
+                ClientId = client.Id,
                 Cost = 1100,
                 Duration = SessionDuration.OneAcademicHour,
                 Message = "",
@@ -117,9 +136,16 @@ namespace BBSK_Psychologists.Tests
                 OrderPaymentStatus = OrderPaymentStatus.Paid,
                 OrderStatus = OrderStatus.Created,
                 PayDate = DateTime.Now,
-                PsychologistId = 2,
+                PsychologistId = psychologist.Id,
                 SessionDate = DateTime.Now
             };
+
+            Order expectedOrder = _mapper.Map<Order>(givenRequest);
+
+            expectedOrder.Client = client;
+            expectedOrder.Psychologist = psychologist;
+
+            _ordersService.Setup(c => c.AddOrder(expectedOrder, _claimModel));
 
             //when
             var actual = _sut.AddOrder(givenRequest);
@@ -129,16 +155,20 @@ namespace BBSK_Psychologists.Tests
 
             Assert.AreEqual(StatusCodes.Status201Created, actualResult.StatusCode);
 
-            _ordersService.Verify(c => c.AddOrder(It.IsAny<Order>(), It.IsAny<ClaimModel>()), Times.Once);
+            _ordersService.Verify(c => c.AddOrder(expectedOrder, _claimModel), Times.Once);
         }
 
         [Test]
         public void DeleteOrderById_ValidIdPassed_NoContentReceived()
         {
             //given
-            Order givenOrder = new();
+            Order givenOrder = OrdersHelper.GetOrder();
 
-            _ordersService.Setup(c => c.DeleteOrder(givenOrder.Id, It.IsAny<ClaimModel>()));
+            _sut.ControllerContext.HttpContext.User = OrdersHelper.GetUser("go@v.no", Role.Manager, 58);
+            _claimModel.Role = Role.Manager;
+
+
+            _ordersService.Setup(c => c.DeleteOrder(givenOrder.Id, _claimModel));
 
             //when
             var actual = _sut.DeleteOrderById(givenOrder.Id);
@@ -148,7 +178,7 @@ namespace BBSK_Psychologists.Tests
 
             Assert.AreEqual(StatusCodes.Status204NoContent, actualResult.StatusCode);
 
-            _ordersService.Verify(c => c.DeleteOrder(It.IsAny<int>(), _claimModel), Times.Once);
+            _ordersService.Verify(c => c.DeleteOrder(givenOrder.Id, _claimModel), Times.Once);
         }
 
         [Test]
@@ -157,13 +187,16 @@ namespace BBSK_Psychologists.Tests
             //given
             Order givenOrder = OrdersHelper.GetOrder();
 
+            _sut.ControllerContext.HttpContext.User = OrdersHelper.GetUser("go@v.no", Role.Manager, 58);
+            _claimModel.Role = Role.Manager;
+
             var givenRequest = new OrderStatusPatchRequest() 
             { 
                 OrderPaymentStatus = OrderPaymentStatus.MoneyReturned,
                 OrderStatus = OrderStatus.Cancelled
             };
 
-            _ordersService.Setup(c => c.UpdateOrderStatuses(givenOrder.Id, givenRequest.OrderStatus, givenRequest.OrderPaymentStatus, It.IsAny<ClaimModel>()));
+            _ordersService.Setup(c => c.UpdateOrderStatuses(givenOrder.Id, givenRequest.OrderStatus, givenRequest.OrderPaymentStatus, _claimModel));
 
             //when
             var actual = _sut.UpdateOrderStatusById(givenOrder.Id, givenRequest);
@@ -173,7 +206,7 @@ namespace BBSK_Psychologists.Tests
 
             Assert.AreEqual(StatusCodes.Status204NoContent, actualResult.StatusCode);
 
-            _ordersService.Verify(c => c.UpdateOrderStatuses(givenOrder.Id, givenRequest.OrderStatus, givenRequest.OrderPaymentStatus, It.IsAny<ClaimModel>()), Times.Once);
+            _ordersService.Verify(c => c.UpdateOrderStatuses(givenOrder.Id, givenRequest.OrderStatus, givenRequest.OrderPaymentStatus, _claimModel), Times.Once);
         }
     }
 }
