54d36ccf2743605f8746bf36f5b90dc54722c813
Daria Shadrina <darinashadrina@yandex.ru>
2022-08-11 18:29:07 +0300

    Fix bugs

--- BBSK_Psycho.DataLayer/Repositories/Interfaces/IPsychologistsRepository.cs
+++ BBSK_Psycho.DataLayer/Repositories/Interfaces/IPsychologistsRepository.cs
@@ -7,12 +7,12 @@ public interface IPsychologistsRepository
     {
         Psychologist? GetPsychologist(int id);
         List<Psychologist> GetAllPsychologists();
-        public Comment AddCommentToPsyhologist(Comment comment, int psychologistId);
+        Comment AddCommentToPsyhologist(Comment comment, int psychologistId);
         List<Comment> GetCommentsByPsychologistId(int id);
         List<Order> GetOrdersByPsychologistsId(int id);
         int AddPsychologist(Psychologist psychologist);
         void UpdatePsychologist(Psychologist psychologist, int id);
         void DeletePsychologist(int id);
 
-        public Task<Psychologist?> GetPsychologistByEmail(string email);
+        Task<Psychologist?> GetPsychologistByEmail(string email);
     }
