402dd0c5cecb8852d215501266d61ce91141a589
Lev <leva.100s@gmail.com>
2022-08-16 22:34:17 +0300

    fix bug in date session

--- BBSK_Psychologists/Infrastructure/ApiErrorMessage.cs
+++ BBSK_Psychologists/Infrastructure/ApiErrorMessage.cs
@@ -22,6 +22,7 @@
         public const string MessageIsRequired = "Вы не описали свои проблемы";
         public const string SessionDateIsRequired = "Вы не выбрали дату сессии";
         public const string InvalidDate = "Ошибка в дате рождения";
+        public const string InvalidSessionDate = "Ошибка в дате сеанса";
         public const string InvalidDateFormat = "Не верный формат даты";
         public const string InvalidPhoneNumber = "Не верный формат номера телефона";
         public const string OrderDateIsRequired = "Не указана дата заказа";
