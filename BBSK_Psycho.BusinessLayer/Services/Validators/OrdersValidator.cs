83faf8722aaf568a5d8f47fdad2bb5b0dbbecda9
Emil <intestinal_d@rambler.ru>
2022-08-23 04:21:32 +0300

    Changed namespace

--- BBSK_Psycho.BusinessLayer/Services/Validators/OrdersValidator.cs
+++ BBSK_Psycho.BusinessLayer/Services/Validators/OrdersValidator.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BBSK_Psycho.BusinessLayer.Services
+namespace BBSK_Psycho.BusinessLayer.Services.Validators
 {
     public class OrdersValidator : IOrdersValidator
     {
